be970a45cd06fcc3be4abb0f709dc7159b0311ed
JulioDalmau <dalmau.ju@gmail.com>
2022-12-05 11:07:05 -0300

    Remove ignored files

--- server/routes/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ProfileController = require("../controller/profile.controller");
-
-router.get("/:id", ProfileController.getUser);
-
-router.put("/:id", ProfileController.editUser);
-
-module.exports = router;
--- server/routes/reports.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ReportsController = require("../controller/reports.controller");
-
-router.get("/", ReportsController.getUsers);
-router.get('/search/:id', ReportsController.searchUser)
-
-module.exports = router;
