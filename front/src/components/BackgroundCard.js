a043ec356276d74addc8904c902452c9c3407137
JulioDalmau <dalmau.ju@gmail.com>
2022-12-28 23:56:16 -0300

    add BackgroundCard

---
+++ front/src/components/BackgroundCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const BackgroundCard = () => {
+  return (
+    <>
+    <div className="absolute bg-white w-11/12 h-3/4 rounded-t-3xl left-4 top-36
+    lg:left-[22%] lg:top-[4%] lg:w-[75%] lg:h-[91%] lg:rounded-3xl     
+    xl:left-[17%] xl:top-[3%] xl:w-[81%] xl:h-[95%] xl:rounded-3xl">
+        <div className="absolute bg-gray-100 rounded-t-3xl 
+        lg:w-full lg:h-[25%]
+        xl:w-full xl:h-[25%]" />
+    </div>
+    </>
+  )
+}
+
