64a4f7b4c66cb7120ac9d6aa2f6793a3a56b3ca4
JulioDalmau <dalmau.ju@gmail.com>
2023-01-05 23:22:28 -0300

    BackgroundCard updated

--- front/src/components/BackgroundCard.js
+++ front/src/components/BackgroundCard.js
@@ -4,9 +4,13 @@ export const BackgroundCard = () => {
   return (
     <>
     <div className="absolute bg-white w-11/12 h-3/4 rounded-t-3xl left-4 top-36
+    sm:left-[28%] sm:top-[8%] sm:w-[69%] sm:h-[88%] sm:rounded-3xl     
+    md:left-[26%] md:top-[6%] md:w-[70%] md:h-[90%] md:rounded-3xl     
     lg:left-[22%] lg:top-[4%] lg:w-[75%] lg:h-[91%] lg:rounded-3xl     
     xl:left-[17%] xl:top-[3%] xl:w-[81%] xl:h-[95%] xl:rounded-3xl">
         <div className="absolute bg-gray-100 rounded-t-3xl 
+        sm:w-full sm:h-[22%]
+        md:w-full md:h-[24%]
         lg:w-full lg:h-[25%]
         xl:w-full xl:h-[25%]" />
     </div>
