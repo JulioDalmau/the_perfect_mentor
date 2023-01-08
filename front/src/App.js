b8366a5bc126fb0de09ba0ee43a2979aeac629aa
JulioDalmau <dalmau.ju@gmail.com>
2023-01-08 16:53:03 -0300

    App route updated

--- front/src/App.js
+++ front/src/App.js
@@ -2,26 +2,33 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
-import Form from "./pages/Form";
+import Skills from "./pages/Skills";
 import Login from "./pages/Login";
 import Users from "./pages/Users";
 import Reports from "./pages/Reports";
 import Stadistics from "./pages/Stadistics";
 import Profile from "./pages/Profile";
+import Result from "./pages/Result";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const user = useSelector((state) => state.user);
+
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/stadistics" element={<Stadistics />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/result/:result" element={<Result />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/stadistics" element={<Stadistics />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
     </>
   );
 }
