fafb88a10d36eec0b6095341fcaa48f4d586d043
JulioDalmau <dalmau.ju@gmail.com>
2023-01-12 18:34:54 -0300

    Utils updated

--- server/utils/auth.utils.js
+++ server/utils/auth.utils.js
@@ -4,9 +4,12 @@ const jwt = require('jsonwebtoken')
 const validateToken = (token) => {
     return jwt.verify(token, process.env.SECRET)
 }
-
-const generateToken = (payload) => {
-    return jwt.sign(payload, process.env.SECRET, { expiresIn: '7d' })
+const generateToken = (payload /* id */) => {
+    console.log("por aca paso",payload)
+    const token = jwt.sign(/* payload */ /* {id} */{user: payload}, process.env.SECRET, { expiresIn: '7d' })
+    console.log("Este es el token",token)
+    return token
 }
+
 
 module.exports = { validateToken, generateToken }
