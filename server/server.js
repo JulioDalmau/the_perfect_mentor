8eb0ef9e0f70ad7e74e47a0d66a880cb525e4155
JulioDalmau <dalmau.ju@gmail.com>
2023-01-13 17:38:00 -0300

    Server updated

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
-const routes = require("./routes");
 const app = express();
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
-require("./config/db");
 require("dotenv").config();
+require("./config/db");
 
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use("/api", routes);
 
 app.listen(process.env.PORT, () => {
