6a86e97cc4e3cb2529e5cf396fdb26eb27a35967
JulioDalmau <dalmau.ju@gmail.com>
2023-01-14 13:51:24 -0300

    remove strictmode

--- front/src/index.js
+++ front/src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./state/store";
+import { Provider } from 'react-redux'
+import store from './state/store'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+    <BrowserRouter>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
-  </React.StrictMode>
+    </BrowserRouter>
 );
